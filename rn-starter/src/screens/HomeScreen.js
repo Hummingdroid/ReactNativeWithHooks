bc903b516deefe3f7b2e3f07ebaa419b90378e39
Jason Green <Hummingdroid@gmail.com>
2020-11-11 08:31:01 -0700

    Updated Comments

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,9 +1,14 @@
-import React from "react"; // This all about how different components work together
+import React from "react"; // This gets you the React library. 
+// This is also known as creating a JavaScript Object to be used to make different components work together. 
+// Its NOT about rendering stuff. Like using React.createElement() etc...
+
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native"; // Use specific things from 'react-native' library. 
+// This creates a JavaScript object to be used to allow for access to Text, StyleSheet etc.
 // This is the library that makes things display on the screen
-// Button is least common used. You will most of the time use TochableOpacity
+// Button is least common used. You will most of the time use TochableOpacity.
 
 
+// Creating a new Component. NOT the same thing as creating a component class!!! like class DoSomething extends React.Component
 // Calling props.navigation explicitly for brevity.
 const HomeScreen = ({ navigation }) => {
     return (
